5a58b5a32004b60c20105c971bef7e427aa62680
yy0608 <447462224@qq.com>
2018-03-31 17:51:17 +0800

    初始化

---
+++ pages/test/multi-upload.js
@@ -0,0 +1,51 @@
+import { origin } from '../../config.js'
+
+Page({
+
+  data: {
+    uploadData: [ // type: 1为文本，2为图片
+      {
+        type: 1,
+        value: '文本内容1'
+      },
+      {
+        type: 1,
+        value: '文本内容1'
+      },
+      {
+        type: 2,
+        value: 'http://img.wsweat.cn/common/nopic.jpg'
+      },
+      {
+        type: 1,
+        value: '文本内容1'
+      },
+      {
+        type: 2,
+        value: 'http://img.wsweat.cn/common/nopic.jpg'
+      },
+      {
+        type: 2,
+        value: 'http://img.wsweat.cn/common/nopic.jpg'
+      },
+      {
+        type: 1,
+        value: '文本内容1'
+      }
+    ]
+  },
+  onLoad () {
+    console.log(origin)
+    wx.request({
+      // url: origin + '/employ/merchant_shops',
+      url: origin + '/employ/near_shops',
+      method: 'get',
+      data: {
+        location: '114.001432, 22.681253'
+      },
+      success: res => {
+        console.log(res.data)
+      }
+    })
+  }
+})
